123c74024b22340abeb4f9e5a7ead6d658314de0
WendySajfer <81486052+WendySajfer@users.noreply.github.com>
2021-09-10 14:30:30 +0300

    Update main.cpp

--- Проект1/main.cpp
+++ Проект1/main.cpp
@@ -6,11 +6,103 @@ using namespace std;
 
 class ROBOT {
 public:
-	int Field_state[10][10]; // состояние поля
-	int x; // координата по горизонтали
-	int y; // координата по вертикали
-	int direct; // направление (0 - вправо, 1 - вниз, 2 - вправо, 3 - вверх)
+	int Field_state[10][10]; // СЃРѕСЃС‚РѕСЏРЅРёРµ РїРѕР»СЏ
+	int x; // РєРѕРѕСЂРґРёРЅР°С‚Р° РїРѕ РіРѕСЂРёР·РѕРЅС‚Р°Р»Рё
+	int y; // РєРѕРѕСЂРґРёРЅР°С‚Р° РїРѕ РІРµСЂС‚РёРєР°Р»Рё
+	int direct; // РЅР°РїСЂР°РІР»РµРЅРёРµ (0 - РІРЅРёР·, 1 - РІР»РµРІРѕ, 2 - РІРІРµСЂС…, 3 - РІРїСЂР°РІРѕ)
+	int markers;
 	
+	void To_the_left(){
+		if(direct!=3) direct++;
+		else direct = 0;
+		cout << "Command completed. Coordinates (" << x << ";" << y << "); Direction: " << direct << "; Markers: " << markers << "; Coordinate state: " << Field_state[x][y] << "." << endl;
+	}
+	void To_the_right(){
+		if(direct!=0) direct--;
+		else direct = 3;
+		cout << "Command completed. Coordinates (" << x << ";" << y << "); Direction: " << direct << "; Markers: " << markers << "; Coordinate state: " << Field_state[x][y] << "." << endl;
+	}
+	void Move(){
+		int buf_x = x;
+		int buf_y = y;
+		Switch(direct){
+			case 0:
+			buf_y ++;
+			break;
+			case 1:
+			buf_x --;
+			break;
+			case 2:
+			buf_y --;
+			break;
+			case 3:
+			buf_x ++;
+			break;
+			default:
+			cout << "Unknown direction." << endl;
+		}
+		if (buf_x < 0 || buf_x > 9 || buf_y < 0 || buf_y > 9) {
+			cout << "Can't go outside the field." << endl;
+			break;
+		}
+		else if (Field_state[buf_x][buf_y] == 2){
+			cout ("Impossible, an obstacle in the way.") << endl;
+			break;
+		     }
+		     else {
+			     x = buf_x;
+			     y = buf_y;
+			     cout << "Command completed. Coordinates (" << x << ";" << y << "); Direction: " << direct << "; Markers: " << markers << "; Coordinate state: " << Field_state[x][y] << "." << endl;
+		     }
+	}
+	void Take_marker(){
+		if (Field_state[x][y] != 1){
+			cout << "There is no marker here." << endl;
+			break;
+		}
+		else {
+			markers++;
+			Field_state[x][y] = 0;
+			cout << "Command completed. Coordinates (" << x << ";" << y << "); Direction: " << direct << "; Markers: " << markers << "; Coordinate state: " << Field_state[x][y] << "." << endl;
+		}
+	}
+	void Put_marker(){
+		if(markers == 0){
+			cout << "The robot has no markers." << endl;
+			break;
+		}
+		else if(Field_state[x][y] != 0){
+			cout << "There is already a marker here." << endl;
+			break;
+		}
+		else {
+			Field_state[x][y] = 1;
+			markers--;
+			cout << "Command completed. Coordinates (" << x << ";" << y << "); Direction: " << direct << "; Markers: " << markers << "; Coordinate state: " << Field_state[x][y] << "." << endl;
+		}
+	}
+	void Command(int n){
+		Switch(n){
+			case 1:
+			To_the_left();
+			break;
+			case 2:
+			To_the_right();
+			break;
+			case 11:
+			Move();
+			break;
+			case 21:
+			Take_marker();
+			break;
+			case 21:
+			Put_marker();
+			break;
+			default:
+			cout << "Unknown command." << endl;
+		}
+		}
+	}
 };
 
 int main(int argc, char* argv[]) {
@@ -22,53 +114,53 @@ int main(int argc, char* argv[]) {
 	robot.y = 0;
 	robot.direct = 0;
 	
-	//Создаем файловый поток и связываем его с файлом
+	//РЎРѕР·РґР°РµРј С„Р°Р№Р»РѕРІС‹Р№ РїРѕС‚РѕРє Рё СЃРІСЏР·С‹РІР°РµРј РµРіРѕ СЃ С„Р°Р№Р»РѕРј
 	ifstream in("input.txt");
 	ofstream out("output.txt");
 
 	if (in.is_open() && out.is_open())
 	{
-		//Если открытие файла прошло успешно
+		//Р•СЃР»Рё РѕС‚РєСЂС‹С‚РёРµ С„Р°Р№Р»Р° РїСЂРѕС€Р»Рѕ СѓСЃРїРµС€РЅРѕ
 
-		//Вначале посчитаем сколько чисел в файле
-		int count = 0;// число чисел в файле
-		int temp;//Временная переменная
+		//Р’РЅР°С‡Р°Р»Рµ РїРѕСЃС‡РёС‚Р°РµРј СЃРєРѕР»СЊРєРѕ С‡РёСЃРµР» РІ С„Р°Р№Р»Рµ
+		int count = 0;// С‡РёСЃР»Рѕ С‡РёСЃРµР» РІ С„Р°Р№Р»Рµ
+		int temp;//Р’СЂРµРјРµРЅРЅР°СЏ РїРµСЂРµРјРµРЅРЅР°СЏ
 
-		while (!in.eof())// пробегаем пока не встретим конец файла eof
+		while (!in.eof())// РїСЂРѕР±РµРіР°РµРј РїРѕРєР° РЅРµ РІСЃС‚СЂРµС‚РёРј РєРѕРЅРµС† С„Р°Р№Р»Р° eof
 		{
 			in >> temp;
 			count++;
 		}
-		//посчитаем число пробелов до знака перевода на новую строку 
+		//РїРѕСЃС‡РёС‚Р°РµРј С‡РёСЃР»Рѕ РїСЂРѕР±РµР»РѕРІ РґРѕ Р·РЅР°РєР° РїРµСЂРµРІРѕРґР° РЅР° РЅРѕРІСѓСЋ СЃС‚СЂРѕРєСѓ 
 
-		//Вначале переведем каретку в потоке в начало файла
+		//Р’РЅР°С‡Р°Р»Рµ РїРµСЂРµРІРµРґРµРј РєР°СЂРµС‚РєСѓ РІ РїРѕС‚РѕРєРµ РІ РЅР°С‡Р°Р»Рѕ С„Р°Р№Р»Р°
 		in.seekg(0, ios::beg);
 		in.clear();
 
-		//Число пробелов в первой строчке вначале равно 0
+		//Р§РёСЃР»Рѕ РїСЂРѕР±РµР»РѕРІ РІ РїРµСЂРІРѕР№ СЃС‚СЂРѕС‡РєРµ РІРЅР°С‡Р°Р»Рµ СЂР°РІРЅРѕ 0
 		int count_space = 0;
 		char symbol;
-		while (!in.eof())//на всякий случай цикл ограничиваем концом файла
+		while (!in.eof())//РЅР° РІСЃСЏРєРёР№ СЃР»СѓС‡Р°Р№ С†РёРєР» РѕРіСЂР°РЅРёС‡РёРІР°РµРј РєРѕРЅС†РѕРј С„Р°Р№Р»Р°
 		{
-			//теперь нам нужно считывать не числа, а посимвольно считывать данные
-			in.get(symbol);//считали текущий символ
-			if (symbol == ' ') count_space++;//Если это пробел, то число пробелов увеличиваем
-			if (symbol == '\n') break;//Если дошли до конца строки, то выходим из цикла
+			//С‚РµРїРµСЂСЊ РЅР°Рј РЅСѓР¶РЅРѕ СЃС‡РёС‚С‹РІР°С‚СЊ РЅРµ С‡РёСЃР»Р°, Р° РїРѕСЃРёРјРІРѕР»СЊРЅРѕ СЃС‡РёС‚С‹РІР°С‚СЊ РґР°РЅРЅС‹Рµ
+			in.get(symbol);//СЃС‡РёС‚Р°Р»Рё С‚РµРєСѓС‰РёР№ СЃРёРјРІРѕР»
+			if (symbol == ' ') count_space++;//Р•СЃР»Рё СЌС‚Рѕ РїСЂРѕР±РµР», С‚Рѕ С‡РёСЃР»Рѕ РїСЂРѕР±РµР»РѕРІ СѓРІРµР»РёС‡РёРІР°РµРј
+			if (symbol == '\n') break;//Р•СЃР»Рё РґРѕС€Р»Рё РґРѕ РєРѕРЅС†Р° СЃС‚СЂРѕРєРё, С‚Рѕ РІС‹С…РѕРґРёРј РёР· С†РёРєР»Р°
 		}
 
-		//Опять переходим в потоке в начало файла
+		//РћРїСЏС‚СЊ РїРµСЂРµС…РѕРґРёРј РІ РїРѕС‚РѕРєРµ РІ РЅР°С‡Р°Р»Рѕ С„Р°Р№Р»Р°
 		in.seekg(0, ios::beg);
 		in.clear();
 
 		if (count == 100 && count_space == 9) {
-			//Считаем матрицу из файла
+			//РЎС‡РёС‚Р°РµРј РјР°С‚СЂРёС†Сѓ РёР· С„Р°Р№Р»Р°
 			for (int i = 0; i < 10; i++)
 				for (int j = 0; j < 10; j++)
 					in >> robot.Field_state[i][j];
 		}
 
 
-		//Выведем матрицу
+		//Р’С‹РІРµРґРµРј РјР°С‚СЂРёС†Сѓ
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				out << robot.Field_state[i][j] << " ";
@@ -79,12 +171,12 @@ int main(int argc, char* argv[]) {
 		}
 
 
-		in.close();//под конец закроем файла
+		in.close();//РїРѕРґ РєРѕРЅРµС† Р·Р°РєСЂРѕРµРј С„Р°Р№Р»Р°
 		out.close();
 	}
 	else
 	{
-		//Если открытие файла прошло не успешно
+		//Р•СЃР»Рё РѕС‚РєСЂС‹С‚РёРµ С„Р°Р№Р»Р° РїСЂРѕС€Р»Рѕ РЅРµ СѓСЃРїРµС€РЅРѕ
 		cout << "Files not found.";
 	}
 	system("pause");
